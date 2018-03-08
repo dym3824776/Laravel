5d0e3227d96188a7f7584897853e71d089c4198e
dym <549574603@qq.com>
2018-03-08 07:08:39 +0000

    增加用户注册

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,5 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 
-Route::get('signup','UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
