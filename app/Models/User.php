7a7067f3174a7df71429933178c5c8dc90b4713f
邓宇鸣 <549574603@qq.com>
2019-02-28 16:40:11 +0800

    增加粉丝关系

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,19 +59,23 @@ class User extends Authenticatable
 		return $this->belongsToMany(User::class,'followers','user_id','follower_id');
     }
 
+
     public function followings() {
 		return $this->belongsToMany(User::class,'followers','follower_id','user_id');
     }
 
-    public function follow($user_ids) {
-		if(is_array($user_ids)) {
+
+	public function follow($user_ids)
+	{
+		if (!is_array($user_ids)) {
 			$user_ids = compact('user_ids');
 		}
-		return $this->followings()->sync($user_ids,false);
-    }
+		$this->followings()->sync($user_ids, false);
+	}
+
 
     public function unfollow($user_ids) {
-		if(is_array($user_ids)) {
+		if(!is_array($user_ids)) {
 			$user_ids = compact('user_ids');
 		}
 		return $this->followings()->detach($user_ids);
--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -1,17 +1,17 @@
-<li  id="status-{{ $status->id }}">
+<li class="media mt-4 mb-4">
 	<a href="{{ route('users.show', $user->id )}}">
-		<img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+		<img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="mr-3 gravatar"/>
 	</a>
-	<span class="media-body">
+	<div class="media-body">
 		<h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
 		{{ $status->content }}
-	</span>
+	</div>
 
 	@can('destroy', $status)
 		<form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您确定要删除本条微博吗？');">
 			{{ csrf_field() }}
 			{{ method_field('DELETE') }}
-			<button type="submit" class="btn btn-sm btn-danger status-delete-btn">删除</button>
+			<button type="submit" class="btn btn-sm btn-danger">删除</button>
 		</form>
 	@endcan
 </li>
