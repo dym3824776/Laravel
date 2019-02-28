0c9331be67360d62aafa474337334ac4b4be517d
邓宇鸣 <549574603@qq.com>
2019-02-28 17:03:55 +0800

    关注和粉丝列表页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -132,4 +132,17 @@ class UsersController extends Controller
 		return redirect()->route('users.show', [$user]);
 	}
 
+	public function followings(User $user)
+	{
+		$users = $user->followings()->paginate(30);
+		$title = $user->name . '关注的人';
+		return view('users.show_follow', compact('users', 'title'));
+	}
+
+	public function followers(User $user)
+	{
+		$user = $user->followers()->paginate(30);
+		$title = $user->name . '的粉丝';
+		return view('users.show_follow',compact('user','title'));
+	}
 }
